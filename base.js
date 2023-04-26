adcc27a8d308a0e0b82a43e87d65f2a37c75e3c7
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-04-25 22:03:40 -0300

    first commit

---
+++ base.js
@@ -0,0 +1,23 @@
+const texto = `João trouxe flores para sua amada namorada em 10 de janierio de 1970, Maria era o nome dela.
+
+Foi um ano excelente na vida de joão. Teve 5 filhos, todos adultos atualmente.
+maria, hoje sua esposa, ainda faz aquele café com pão de queijo nas tardes de domingo. Também né! Sendi a boa mineira que é, nunca esquece seu famoso pão de queijo.
+Não canso de ouvir a Maria:
+"Jooooooãooooo, o café tá prontinho aqui. Veeemm"!
+`
+
+const arquivos =[
+    'Atenção.jpeg',
+    'FOTO.jpeg',
+    'Meu gatinho.jpg',
+    'Meu gatinho.JPG',
+    'Meu gatinho.JPEG',
+    'Meu gatinho.JPEeeEEEeeeEEG',
+    'Marido.png',
+    'lista de compras.txt'
+
+
+]
+module.exports = {
+    texto, arquivos
+}
